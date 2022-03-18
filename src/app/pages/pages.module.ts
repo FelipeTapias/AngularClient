192dc73220993cf5b754ee84ee6268cfe6a17e82
FelipeTapias <tapiastuberquiados@gmail.com>
2022-03-17 19:01:32 -0500

    I created the form-register component, the html of navbar, too

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 
-
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule,
     PagesRoutingModule
